2659fa42c0d31c3489ce42b9e4655ed930033641
jamestkelly <jimkelly.t@outlook.com>
2022-07-24 18:20:24 +1000

    Added basic Binary Tree functionality [TEXT]

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BinaryTree from './utils/BinaryTree';
+
+const binaryTree = new BinaryTree();
+let values = [10, 20, 30, 40, 5, 8, 3, 9];
+
+for (let i = 0; i < values.length; i++) {
+  binaryTree.insert(values[i]);
+}
+
+console.log("Test - 'Tree Contains':");
+console.log(binaryTree.contains(30));
+
+console.log("Test - 'findMin()':");
+console.log(binaryTree.findMin());
+
+console.log("Test - 'findMax()'");
+console.log(binaryTree.findMax());
 
 function App() {
   return (
